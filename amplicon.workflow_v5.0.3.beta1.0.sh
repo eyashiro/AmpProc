1f8953c99caf9e2aea0f5d8f5ed63fe25f016a54
Kasper Skytte Andersen <knaldhat@gmail.com>
2019-02-11 15:29:18 +0100

    pretty-print elapsed time when done

--- amplicon.workflow_v5.0.3.beta1.0.sh
+++ amplicon.workflow_v5.0.3.beta1.0.sh
@@ -1341,4 +1341,5 @@ echo "Paired-end read processing is done. Enjoy."
 date
 echo ""
 
-
+duration=$(printf '%02dh:%02dm:%02ds\n' $(($SECONDS/3600)) $(($SECONDS%3600/60)) $(($SECONDS%60)))
+echoWithDate "Done in: $duration!"
